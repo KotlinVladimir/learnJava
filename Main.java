1831c67dbde33f50f90383ae371385b4fe6d0755
Владимир <78254842+KotlinVladimir@users.noreply.github.com>
2023-08-27 09:06:12 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -5,23 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("¬ведите размер 1 массива: ");
+        System.out.println("Enter the size of 1 array:");
         int size = sc.nextInt();
         int[] arrayFirst = new int[size];
-        System.out.println("¬ведите " + size + " чисел: ");
+        System.out.println("Enter " + size + " numbers:");
         for (int i = 0; i < size; i++) {
             arrayFirst[i] = sc.nextInt();
         }
-        System.out.println("¬ведите размер 2 массива: ");
+        System.out.println("Enter the size of 2 array:");
         int sizeSecond = sc.nextInt();
         int[] arraySecond= new int[sizeSecond];
-        System.out.println("¬ведите " + sizeSecond + " чисел: ");
+        System.out.println("Enter " + sizeSecond + " numbers:");
         for (int j = 0; j < sizeSecond; j++) {
             arraySecond[j] = sc.nextInt();
         }
         int[] united;
         united = new int[arrayFirst.length + arraySecond.length];
-        System.out.println("—оздаю массив размером суммы длин 2 предыдущих");
+        System.out.println("Creating an array with the size of the sum of the lengths of the previous 2");
         System.out.println(Arrays.toString(united));
         int count = 0;
 
@@ -29,13 +29,13 @@ public class Main {
             united[i] = arrayFirst[i];
             count++;
         }
-        System.out.println("ƒобавл€ю элементы из 1 массива");
+        System.out.println("Adding elements from 1 array");
         System.out.println(Arrays.toString(united));
         for (int j = 0; j < arraySecond.length; j++) {
             united[count] = arraySecond[j];
             count++;
         }
-        System.out.println("ƒобавл€ю элементы из 2 массива");
+        System.out.println("Adding elements from 2 array");
         System.out.println(Arrays.toString(united));
     }
 }
